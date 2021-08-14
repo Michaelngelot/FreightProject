53452488f454a1ac0ee1e74be2a9f5c3c5c4e56e
Roland <rolanddadia9@gmail.com>
2021-08-14 07:20:49 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MainScreen(),
