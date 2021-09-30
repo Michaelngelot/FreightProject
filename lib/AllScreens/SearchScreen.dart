d42b8fb3b9e76d1858cd554ad58dcf0337a211c0
Roland <rolanddadia9@gmail.com>
2021-09-30 17:11:34 +0100

    Ride request

--- lib/AllScreens/SearchScreen.dart
+++ lib/AllScreens/SearchScreen.dart
@@ -14,183 +14,184 @@ class SearchScreen extends StatefulWidget {
   _SearchScreenState createState() => _SearchScreenState();
 }
 
+
 class _SearchScreenState extends State<SearchScreen>
 {
-  TextEditingController pickUpTextEditingController = TextEditingController();
-  TextEditingController dropOffTextEditingController = TextEditingController();
-  List<PlacePredictions> placePredictionList = [];
+TextEditingController pickUpTextEditingController = TextEditingController();
+TextEditingController dropOffTextEditingController = TextEditingController();
+List<PlacePredictions> placePredictionList = [];
 
-  @override
-  Widget build(BuildContext context)
-  {
-    String placeAddress = Provider.of<AppData>(context).pickUpLocation!.placeName;
-    pickUpTextEditingController.text = placeAddress;
+@override
+Widget build(BuildContext context)
+{
+  String placeAddress = Provider.of<AppData>(context).pickUpLocation!.placeName;
+  pickUpTextEditingController.text = placeAddress;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 215.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black,
-                  blurRadius: 6.0,
-                  spreadRadius: 0.5,
-                  offset: Offset(0.7, 0.7),
-                ),
-              ],
-            ),
-
-            child: Padding(
-              padding: EdgeInsets.only(left: 25.0, top: 25.0, right: 25.0, bottom: 20.0),
-              child: Column(
-                children: [
-
-                  SizedBox(height: 25.0),
-                  Stack(
-                    children: [
-                      GestureDetector(
-                        onTap:()
-                        {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(
-                            Icons.arrow_back
-                        ),
-                      ),
-
-                      Center(
-                        child: Text("Set Drop Off", style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 16.0),
-
-                  Row(
-                    children: [
-                      Image.asset("images/pickicon.png", height: 16.0, width: 16.0,),
-
-                      SizedBox(width: 18.0,),
-
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(3.0),
-                            child: TextField(
-                              controller: pickUpTextEditingController,
-                              decoration: InputDecoration(
-                                hintText: "PickUp Location",
-                                fillColor: Colors.grey[200],
-                                filled: true,
-                                border: InputBorder.none,
-                                isDense: true,
-                                contentPadding: EdgeInsets.only(left: 11.0, top: 8.0, bottom: 8.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 10.0),
-
-                  Row(
-                    children: [
-                      Image.asset("images/desticon.png", height: 16.0, width: 16.0,),
-
-                      SizedBox(width: 18.0,),
-
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(3.0),
-                            child: TextField(
-                              onChanged: (val)
-                              {
-                                findPlace(val);
-                              },
-                              controller: dropOffTextEditingController,
-                              decoration: InputDecoration(
-                                hintText: "Where to?",
-                                fillColor: Colors.grey[200],
-                                filled: true,
-                                border: InputBorder.none,
-                                isDense: true,
-                                contentPadding: EdgeInsets.only(left: 11.0, top: 8.0, bottom: 8.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                ],
+  return Scaffold(
+    body: Column(
+      children: [
+        Container(
+          height: 215.0,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 6.0,
+                spreadRadius: 0.5,
+                offset: Offset(0.7, 0.7),
               ),
-            ),
+            ],
           ),
 
-          //tile for predictions
-          SizedBox(height: 10.0,),
-          (placePredictionList.length > 0)
-              ? Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: ListView.separated(
-                padding: EdgeInsets.all(0.0),
-                itemBuilder:(context,index){
-                  return PredictionTile(placePredictions: placePredictionList[index]);
-                },
-                separatorBuilder: (BuildContext context, int index) => DividerWidget(),
-                itemCount: placePredictionList.length,
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-              ),
+          child: Padding(
+            padding: EdgeInsets.only(left: 25.0, top: 25.0, right: 25.0, bottom: 20.0),
+            child: Column(
+              children: [
+
+                SizedBox(height: 25.0),
+                Stack(
+                  children: [
+                    GestureDetector(
+                      onTap:()
+                      {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                          Icons.arrow_back
+                      ),
+                    ),
+
+                    Center(
+                      child: Text("Set Drop Off", style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 16.0),
+
+                Row(
+                  children: [
+                    Image.asset("images/pickicon.png", height: 16.0, width: 16.0,),
+
+                    SizedBox(width: 18.0,),
+
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: TextField(
+                            controller: pickUpTextEditingController,
+                            decoration: InputDecoration(
+                              hintText: "PickUp Location",
+                              fillColor: Colors.grey[200],
+                              filled: true,
+                              border: InputBorder.none,
+                              isDense: true,
+                              contentPadding: EdgeInsets.only(left: 11.0, top: 8.0, bottom: 8.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10.0),
+
+                Row(
+                  children: [
+                    Image.asset("images/desticon.png", height: 16.0, width: 16.0,),
+
+                    SizedBox(width: 18.0,),
+
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: TextField(
+                            onChanged: (val)
+                            {
+                              findPlace(val);
+                            },
+                            controller: dropOffTextEditingController,
+                            decoration: InputDecoration(
+                              hintText: "Where to?",
+                              fillColor: Colors.grey[200],
+                              filled: true,
+                              border: InputBorder.none,
+                              isDense: true,
+                              contentPadding: EdgeInsets.only(left: 11.0, top: 8.0, bottom: 8.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
             ),
-          )
-              : Container(),
-        ],
-      ),
-    );
-  }
+          ),
+        ),
 
-  void findPlace(String placeName) async
+        //tile for predictions
+        SizedBox(height: 10.0,),
+        (placePredictionList.length > 0)
+            ? Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: ListView.separated(
+              padding: EdgeInsets.all(0.0),
+              itemBuilder:(context,index){
+                return PredictionTile(placePredictions: placePredictionList[index]);
+              },
+              separatorBuilder: (BuildContext context, int index) => DividerWidget(),
+              itemCount: placePredictionList.length,
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+            ),
+          ),
+        )
+            : Container(),
+      ],
+    ),
+  );
+}
+
+void findPlace(String placeName) async
+{
+  if(placeName.length > 1)
   {
-    if(placeName.length > 1)
+    String autoCompleteUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$placeName&key=$mapKey&sessiontoken=1234567890&";
+
+    var res = await RequestAssistant.getRequest(autoCompleteUrl);
+
+    if(res == "failed")
     {
-      String autoCompleteUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$placeName&key=$mapKey&sessiontoken=1234567890&";
+      return;
+    }
 
-      var res = await RequestAssistant.getRequest(autoCompleteUrl);
+    if(res["status"] == "OK")
+    {
+      var predictions = res["predictions"];
 
-      if(res == "failed")
-      {
-        return;
-      }
+      var placesList = (predictions as List).map((e) => PlacePredictions.fromJson(e)).toList();
 
-      if(res["status"] == "OK")
-      {
-        var predictions = res["predictions"];
-
-        var placesList = (predictions as List).map((e) => PlacePredictions.fromJson(e)).toList();
-
-        setState(() {
-          placePredictionList = placesList;
-        });
-      }
+      setState(() {
+        placePredictionList = placesList;
+      });
     }
   }
+}
 }
 
 
